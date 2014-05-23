4265ecc0c2dfbb0a90f97b897b066e450db7eae1
Warrence Oo <warrence@centralsoft.com.my>
2014-05-23 16:30:19 +0800

    Minor changes

--- crud/default/views/create.php
+++ crud/default/views/create.php
@@ -23,9 +23,9 @@ $this->params['breadcrumbs'][] = ['label' => <?= $generator->generateString(Infl
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-create">
-
-    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
-
+    <div class="page-header">
+        <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
+    </div>
     <?= "<?= " ?>$this->render('_form', [
         'model' => $model,
     ]) ?>
