2b5a6d5ba9565a27404845f4bbd9df37d292c20b
zhangyc310 <yqf0215@gmail.com>
2020-02-14 18:42:27 +0800

    select multi type

--- crud/form.php
+++ crud/form.php
@@ -16,3 +16,42 @@ echo $form->field($generator, 'indexWidgetType')->dropDownList([
 ]);
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
+
+if (Yii::$app->request->isPost) {
+    $table_s = $generator->getTableSchema();
+
+    if (empty($table_s)) {
+        return;
+    }
+
+    $columns = $table_s->columns;
+    $cols    = [];
+    foreach ($columns as $key => $val) {
+        $cols[$key] = $val->name;
+
+    }
+    // var_dump($cols);
+    echo $form->field($generator, 'listFields')->checkboxList($cols);
+    if (empty($generator->inputType)) {
+        foreach ($columns as $name => $val) {
+            $generator->inputType[$name] = 1;
+        }
+    }
+    // var_dump($generator->inputType);
+    echo "<div form-group'>";
+    echo '<label control-label help" data-original-title title>Form Fields</label>';
+    echo "<div  class='row'>";
+    foreach ($columns as $name => $val) {
+        $checked = '';
+        if (!empty($generator->formFields) && in_array($name, array_values($generator->formFields))) {
+            $checked = 'checked="checked"';
+            echo "cccc::::" . $name . ":" . "," . $checked . ";;;;";
+        }
+
+        // var_dump($val);
+        echo '<div class="col-lg-9"><input type="checkbox" name="Generator[formFields][]" value="' . $name . '" ' . $checked . '> <label control-label">' . $name . '</label></div>';
+        echo '<div class="col-lg-3">' . Html::dropDownList("Generator[inputType][$name]", $generator->inputType[$name],
+            $generator->fieldTypes(), ['class' => 'form-control']) . '</div>';
+    }
+    echo "</div></div>";
+}
